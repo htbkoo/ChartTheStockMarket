f27fec94dca74f8f566392186cc20480fcdd56af
H.T. Koo <h1017760@connect.hku.hk>
2017-08-24 07:45:35 +0800

    refactored reduxReducers.test.js FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- views/chart-the-stock-market-ui/src/unit-tests/reduxReducers.test.js
+++ views/chart-the-stock-market-ui/src/unit-tests/reduxReducers.test.js
@@ -8,64 +8,73 @@ describe("reduxReducers", function () {
         stocks: []
     };
 
-    [
-        {
-            name: "should return the initial state",
-            params: {
-                state: undefined,
-                action: {}
-            },
+    describe("no action", function () {
+        it("should return the initial state", testReducer({
+            params:
+                {
+                    state: undefined,
+                    action:
+                        {}
+                }
+            ,
             expectedState: initialState
-        },
-        {
-            name: "should handle ADD_STOCK for uninitialized stat",
-            params: {
-                state: undefined,
-                action: {
-                    type: types.ADD_STOCK,
-                    underlyingId: "someId"
-                }
-            },
-            expectedState: {
-                stocks: ["someId"]
-            }
-        },
-        {
-            name: "should handle ADD_STOCK for empty stocks case",
-            params: {
-                state: initialState,
-                action: {
-                    type: types.ADD_STOCK,
-                    underlyingId: "someId"
-                }
-            },
-            expectedState: {
-                stocks: ["someId"]
-            }
-        },
-        {
-            name: "should handle ADD_STOCK for non-empty stocks case",
-            params: {
-                state: {
-                    stocks: ["existing"]
+        }));
+    });
+
+    describe("addStock", function () {
+        [
+            {
+                name: "should handle ADD_STOCK for uninitialized stat",
+                params: {
+                    state: undefined,
+                    action: {
+                        type: types.ADD_STOCK,
+                        underlyingId: "someId"
+                    }
                 },
-                action: {
-                    type: types.ADD_STOCK,
-                    underlyingId: "someId"
+                expectedState: {
+                    stocks: ["someId"]
                 }
             },
-            expectedState: {
-                stocks: ["existing", "someId"]
-            }
-        },
-    ].forEach(testCase =>
-        it(testCase.name, function () {
+            {
+                name: "should handle ADD_STOCK for empty stocks case",
+                params: {
+                    state: initialState,
+                    action: {
+                        type: types.ADD_STOCK,
+                        underlyingId: "someId"
+                    }
+                },
+                expectedState: {
+                    stocks: ["someId"]
+                }
+            },
+            {
+                name: "should handle ADD_STOCK for non-empty stocks case",
+                params: {
+                    state: {
+                        stocks: ["existing"]
+                    },
+                    action: {
+                        type: types.ADD_STOCK,
+                        underlyingId: "someId"
+                    }
+                },
+                expectedState: {
+                    stocks: ["existing", "someId"]
+                }
+            },
+        ].forEach(testCase => it(testCase.name, testReducer(testCase)));
+    });
+
+    function testReducer(testCase) {
+        return function () {
             //    given
             //    when
             let state = reducers(testCase.params.state, testCase.params.action);
 
             //    then
             chai.expect(state).to.deep.equal(testCase.expectedState);
-        })
-    );
+        };
+    }
 });
