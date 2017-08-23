93d197fa306ff2ce4a2a19dfc3e7897ce475bc26
H.T. Koo <h1017760@connect.hku.hk>
2017-08-23 13:58:38 +0800

    added handling of addStock action in reduxReducer and test FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- views/chart-the-stock-market-ui/src/redux/reduxReducers.js
+++ views/chart-the-stock-market-ui/src/redux/reduxReducers.js
@@ -1,7 +1,17 @@
+import types from "./actionTypes"
+
 const initialState = {
     stocks: []
 };
 
 export default function stocks(state = initialState, action) {
-    return state;
+    switch (action.type) {
+        case types.ADD_STOCK: {
+            let newState = {...state};
+            newState.stocks.push(action.underlyingId);
+            return newState;
+        }
+        default:
+            return state;
+    }
 }
--- views/chart-the-stock-market-ui/src/unit-tests/reduxReducers.test.js
+++ views/chart-the-stock-market-ui/src/unit-tests/reduxReducers.test.js
@@ -1,16 +1,36 @@
 import chai from "chai";
 
 import reducers from "../redux/reduxReducers";
+import types from "../redux/actionTypes";
 
 describe("reduxReducers", function () {
+    const initialState = {
+        stocks: []
+    };
+
     it("should return the initial state", function () {
         //    given
-        const expectedState = {
-            stocks: []
-        };
+        const expectedState = initialState;
 
         //    when
         let state = reducers(undefined, {});
+
+        //    then
+        chai.expect(state).to.deep.equal(expectedState);
+    });
+
+    it("should handle ADD_STOCK", function () {
+        //    given
+        const underlyingId = "someId";
+        const expectedState = {
+            stocks: [underlyingId]
+        };
+
+        //    when
+        let state = reducers(initialState, {
+            type: types.ADD_STOCK,
+            underlyingId
+        });
 
         //    then
         chai.expect(state).to.deep.equal(expectedState);
