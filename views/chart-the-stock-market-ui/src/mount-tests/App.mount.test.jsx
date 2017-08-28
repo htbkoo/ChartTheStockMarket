ff5f01e67306edc888ac5b9f20fc536eeefc0a34
H.T. Koo <h1017760@connect.hku.hk>
2017-08-29 07:50:00 +0800

    fixed react mount test after incorporating usage of redux FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- views/chart-the-stock-market-ui/src/mount-tests/App.mount.test.jsx
+++ views/chart-the-stock-market-ui/src/mount-tests/App.mount.test.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import {mount} from 'enzyme';
+import {Provider} from 'react-redux';
+import configureStore from 'redux-mock-store';
 
 import App from '../App.jsx';
 
 require('./setup.jsx');
 
 describe("mount test - App", function () {
-    it('renders without crashing', () => {
-        // const
-        mount(<App/>);
+    it('renders with a mock store without crashing', () => {
+        let initialState = {stocks: []};
+        mount(
+            <Provider store={configureStore()(initialState)}>
+                <App/>
+            </Provider>
+        );
     });
 });
