469248c50ce190075f01424b0712a0e06ab9ffe2
H.T. Koo <h1017760@connect.hku.hk>
2017-09-04 09:27:16 +0800

    grouped testcases in keycodeHelper.test.js FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- views/chart-the-stock-market-ui/src/unit-tests/keycodeHelper.test.js
+++ views/chart-the-stock-market-ui/src/unit-tests/keycodeHelper.test.js
@@ -4,58 +4,62 @@ import keycode from 'keycode';
 import keycodeHelper from '../utils/keycodeHelper';
 
 describe("keycodeHelper", function () {
-    it("should expose ENTER as constant", function () {
-        //    given
-        const expected = keycode("ENTER");
+    describe("constants", function () {
+        it("should expose ENTER as constant", function () {
+            //    given
+            const expected = keycode("ENTER");
 
-        //    when
-        let actual = keycodeHelper.ENTER;
+            //    when
+            let actual = keycodeHelper.ENTER;
 
-        //    then
-        chai.expect(actual).to.equal(expected);
+            //    then
+            chai.expect(actual).to.equal(expected);
+        });
     });
 
-    it("should return true for isEnter('Enter')", function () {
-        //    given
-        const strKeycode = "Enter";
+    describe("isEnter", function () {
+        it("should return true for isEnter('Enter')", function () {
+            //    given
+            const strKeycode = "Enter";
 
-        //    when
-        let expected = keycodeHelper.isEnter(strKeycode);
+            //    when
+            let expected = keycodeHelper.isEnter(strKeycode);
 
-        //    then
-        chai.expect(expected).to.be.true;
-    });
+            //    then
+            chai.expect(expected).to.be.true;
+        });
 
-    it("should return false for isEnter('sthElse')", function () {
-        //    given
-        const strKeycode = "sthElse";
+        it("should return false for isEnter('sthElse')", function () {
+            //    given
+            const strKeycode = "sthElse";
 
-        //    when
-        let expected = keycodeHelper.isEnter(strKeycode);
+            //    when
+            let expected = keycodeHelper.isEnter(strKeycode);
 
-        //    then
-        chai.expect(expected).to.be.false;
-    });
+            //    then
+            chai.expect(expected).to.be.false;
+        });
 
-    it("should return true for isEnter(keycode('enter'))", function () {
-        //    given
-        const numKeycode = keycode("enter");
+        it("should return true for isEnter(keycode('enter'))", function () {
+            //    given
+            const numKeycode = keycode("enter");
 
-        //    when
-        let expected = keycodeHelper.isEnter(numKeycode);
+            //    when
+            let expected = keycodeHelper.isEnter(numKeycode);
 
-        //    then
-        chai.expect(expected).to.be.true;
-    });
+            //    then
+            chai.expect(expected).to.be.true;
+        });
 
-    it("should return false for isEnter(keycode('space'))", function () {
-        //    given
-        const numKeycode = keycode("space");
+        it("should return false for isEnter(keycode('space'))", function () {
+            //    given
+            const numKeycode = keycode("space");
 
-        //    when
-        let expected = keycodeHelper.isEnter(numKeycode);
+            //    when
+            let expected = keycodeHelper.isEnter(numKeycode);
 
-        //    then
-        chai.expect(expected).to.be.false;
+            //    then
+            chai.expect(expected).to.be.false;
+        });
     });
 });
