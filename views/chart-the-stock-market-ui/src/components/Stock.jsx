82521675f9edf43fd8332b2879af745f1c37d670
H.T. Koo <h1017760@connect.hku.hk>
2017-09-27 20:10:11 +0800

    added button in <Stock/> (to add remove stock functionality) FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- views/chart-the-stock-market-ui/src/components/Stock.jsx
+++ views/chart-the-stock-market-ui/src/components/Stock.jsx
@@ -4,7 +4,12 @@ export default class Stock extends Component {
     render() {
         return (
             <div className="Stock">
-                {this.props.stock}
+                <button type="button" className="close" aria-label="Close">
+                    <span aria-hidden="false">&times;</span>
+                </button>
+                <div className="StockText">
+                    {this.props.stock}
+                </div>
             </div>
         );
     }
--- views/chart-the-stock-market-ui/src/unit-tests/Stock.test.jsx
+++ views/chart-the-stock-market-ui/src/unit-tests/Stock.test.jsx
@@ -5,15 +5,28 @@ import chai from '../test-util/chaiWithEnzyme';
 import Stock from '../components/Stock.jsx';
 
 describe("Stock", function () {
-    it('should have child <div>{props.stock}</div>', () => {
+    it('should have child <div class="StockText">{props.stock}</div>', () => {
         // given
         let stock = "stock";
 
         // when
         let stockComponent = shallow(<Stock stock={stock}/>);
-        let div = stockComponent.find('div');
+        let div_StockText = stockComponent.find('div.StockText');
 
         // then
-        chai.expect(div).to.have.text(stock);
+        chai.expect(div_StockText).to.have.text(stock);
+    });
+
+    it('should have child <button class="close"> ', () => {
+        // given
+        let mockOnRemoveStock = "mock";
+
+        // when
+        let stockComponent = shallow(<Stock onRemoveStock={mockOnRemoveStock}/>);
+        let buttonComponent = stockComponent.find('button');
+
+        // then
+        chai.expect(buttonComponent).to.have.length(1);
+        chai.expect(buttonComponent).to.have.className("close");
     });
 });
