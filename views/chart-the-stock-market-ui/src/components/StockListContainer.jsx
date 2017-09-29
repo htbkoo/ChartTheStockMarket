8448ca40d3be02b6eb81d2aa7ecfce106b9b8df4
H.T. Koo <h1017760@connect.hku.hk>
2017-09-29 20:20:27 +0800

    refactored StockListContainer.jsx to reduce duplication FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- views/chart-the-stock-market-ui/src/components/StockListContainer.jsx
+++ views/chart-the-stock-market-ui/src/components/StockListContainer.jsx
@@ -1,21 +1,19 @@
 import {connect} from 'react-redux';
 import * as actions from '../redux/reduxActions';
+import stringHelper from '../utils/stringHelper';
 import StockList from './StockList';
 
 const mapStateToProps = state => ({
     "stocks": state.get("stocks")
 });
 
-export const mapDispatchToProps = dispatch => ({
-    "onRemoveStock": underlyingId => {
-        console.log("onRemoveStock called with: " + underlyingId);
-        dispatch(actions.removeStock(underlyingId));
-    },
-    "onReorderStock": (underlyingId, newPosition) => {
-        console.log("onReorderStock called with: " + [underlyingId, newPosition]);
-        dispatch(actions.reorderStock(underlyingId, newPosition));
-    },
-});
+export const mapDispatchToProps = dispatch => [
+    "onRemoveStock",
+    "onReorderStock"
+].reduce((obj, listenerName) => {
+    obj[listenerName] = (...params) => dispatchWithLogging(listenerName, dispatch, params);
+    return obj;
+}, {});
 
 const StockListContainer = connect(
     mapStateToProps,
@@ -23,3 +21,9 @@ const StockListContainer = connect(
 )(StockList);
 
 export default StockListContainer;
+
+function dispatchWithLogging(listenerName, dispatch, params) {
+    let actionsKey = stringHelper.convert.forReduxActions(listenerName);
+    console.log(`${listenerName} called with: ${params}, thus creating actions[${actionsKey}](${params})`);
+    return dispatch(actions[actionsKey].apply(this, params));
+}
