6ed490cccd0415956a16698bf20be7d2f0d98a6b
H.T. Koo <h1017760@connect.hku.hk>
2017-10-08 18:04:26 +0800

    merging the 2 very similar SortableComponents (SortableList and SortableItem) FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- views/chart-the-stock-market-ui/src/components/SortableComponents.jsx
+++ views/chart-the-stock-market-ui/src/components/SortableComponents.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import propTypes from 'prop-types';
 import {SortableContainer} from 'react-sortable-hoc';
 
-export default class SortableList extends Component {
+export class SortableList extends Component {
     render() {
         return React.createElement(SortableContainer(() => this.props.children), this.props);
     }
---
+++ views/chart-the-stock-market-ui/src/unit-tests/SortableComponents.test.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import chai from '../test-util/chaiWithEnzyme';
+
+import {SortableList} from '../components/SortableComponents.jsx';
+
+describe("SortableComponents", function () {
+    describe("SortableList", function () {
+        it('should render <SortableList><div/></SortableList>> without mocking SortableContainer', () => {
+            // given
+            let children = <div/>;
+
+            // when
+            let wrapper = shallow(<SortableList>{children}</SortableList>);
+
+            // then
+            chai.expect(wrapper).to.contain(children);
+            chai.expect(wrapper).to.not.have.prop('withRef');
+            assertIsSortableList(wrapper);
+        });
+
+        it('should render <SortableList withRef={true}><div/></SortableList>> and the props should be passed down', () => {
+            // given
+            let children = <span/>;
+
+            // when
+            let wrapper = shallow(<SortableList withRef={true}>{children}</SortableList>);
+
+            // then
+            chai.expect(wrapper).to.contain(children);
+            chai.expect(wrapper).to.have.prop('withRef').that.equal(true);
+            assertIsSortableList(wrapper);
+        });
+
+        function assertIsSortableList(wrapper) {
+            const SORTABLE_LIST_ATTRIBUTES = ["axis", "transitionDuration", "pressDelay", "pressThreshold", "distance", "useWindowAsScrollContainer", "hideSortableGhost", "lockToContainerEdges", "lockOffset"];
+            SORTABLE_LIST_ATTRIBUTES.forEach(attribute => chai.expect(wrapper).to.have.prop(attribute));
+        }
+    });
+});
