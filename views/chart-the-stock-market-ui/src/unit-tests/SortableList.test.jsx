84a97cd165868b8381ecf4a89f0d67c744bc906b
H.T. Koo <h1017760@connect.hku.hk>
2017-10-08 12:37:15 +0800

    improved SortableList.test.jsx; added test to assert props passing down FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- views/chart-the-stock-market-ui/src/unit-tests/SortableList.test.jsx
+++ views/chart-the-stock-market-ui/src/unit-tests/SortableList.test.jsx
@@ -14,5 +14,25 @@ describe("SortableList", function () {
 
         // then
         chai.expect(wrapper).to.contain(children);
+        chai.expect(wrapper).to.not.have.prop('withRef');
+        assertIsSortableList(wrapper);
     });
+
+    it('should render <SortableList withRef={true}><div/></SortableList>> and the props should be passed down', () => {
+        // given
+        let children = <span/>;
+
+        // when
+        let wrapper = shallow(<SortableList withRef={true}>{children}</SortableList>);
+
+        // then
+        chai.expect(wrapper).to.contain(children);
+        chai.expect(wrapper).to.have.prop('withRef').that.equal(true);
+        assertIsSortableList(wrapper);
+    });
+
+    function assertIsSortableList(wrapper) {
+        const SORTABLE_LIST_ATTRIBUTES = ["axis", "transitionDuration", "pressDelay", "pressThreshold", "distance", "useWindowAsScrollContainer", "hideSortableGhost", "lockToContainerEdges", "lockOffset"];
+        SORTABLE_LIST_ATTRIBUTES.forEach(attribute => chai.expect(wrapper).to.have.prop(attribute));
+    }
 });
