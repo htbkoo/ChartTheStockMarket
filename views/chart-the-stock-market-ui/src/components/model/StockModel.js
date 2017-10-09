bce5a532027ee6899a4617071da7109479e955da
H.T. Koo <h1017760@connect.hku.hk>
2017-10-09 19:58:04 +0800

    migrated stocks to be accessed by accessors in StockModel FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- views/chart-the-stock-market-ui/src/components/model/StockModel.js
+++ views/chart-the-stock-market-ui/src/components/model/StockModel.js
@@ -1,7 +1,14 @@
 import {List} from 'immutable';
 
 export default class StockModel {
-    constructor(){
-        this.stocks = List();
+    constructor() {
+        let _stocks = List();
+        this.getStocks = () => {
+            return _stocks;
+        };
+
+        this.setStocks = (stocks) => {
+            _stocks = stocks;
+        }
     }
 }
--- views/chart-the-stock-market-ui/src/unit-tests/StockModel.test.js
+++ views/chart-the-stock-market-ui/src/unit-tests/StockModel.test.js
@@ -1,8 +1,11 @@
 import chai from 'chai';
+import chaiImmutable from "chai-immutable";
 
 import StockModel from '../components/model/StockModel';
 
 import {List} from 'immutable';
+
+chai.use(chaiImmutable);
 
 describe("StockModel", function () {
     it("should create StockModel", function () {
@@ -14,12 +17,17 @@ describe("StockModel", function () {
         chai.expect(stockModel).to.be.an.instanceOf(StockModel);
     });
 
-    it("should contains property 'stocks' that is an immutable List", function () {
+    it("should contains property 'stocks' that is an immutable List (exposed as getter and setter)", function () {
         //    given
         //    when
         let stockModel = new StockModel();
 
         //    then
-        chai.expect(stockModel.stocks).to.be.an.instanceOf(List);
+        chai.expect(stockModel.getStocks()).to.be.an.instanceOf(List);
+        chai.expect(stockModel.getStocks()).to.equal(List());
+
+        let newStocks = List.of(1);
+        stockModel.setStocks(newStocks);
+        chai.expect(stockModel.getStocks()).to.equal(newStocks);
     });
 });
