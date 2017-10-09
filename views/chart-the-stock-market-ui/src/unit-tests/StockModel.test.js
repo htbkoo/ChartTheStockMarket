5e8510c99de162b0868d5f727da75f18755e3468
H.T. Koo <h1017760@connect.hku.hk>
2017-10-09 20:14:13 +0800

    formatted and grouped tests in StockModel.test.js FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- views/chart-the-stock-market-ui/src/unit-tests/StockModel.test.js
+++ views/chart-the-stock-market-ui/src/unit-tests/StockModel.test.js
@@ -8,41 +8,45 @@ import {List} from 'immutable';
 chai.use(chaiImmutable);
 
 describe("StockModel", function () {
-    it("should create StockModel", function () {
-        //    given
-        //    when
-        let stockModel = new StockModel();
-
-        //    then
-        chai.expect(stockModel).to.be.an.instanceOf(StockModel);
-    });
-
-    it("should contains property 'stocks' that is an immutable List (exposed as getter and setter)", function () {
-        //    given
-        //    when
-        let stockModel = new StockModel();
-
-        //    then
-        chai.expect(stockModel.getStocks()).to.be.an.instanceOf(List);
-        chai.expect(stockModel.getStocks()).to.equal(List());
-
-        let newStocks = List.of(1);
-        stockModel.setStocks(newStocks);
-        chai.expect(stockModel.getStocks()).to.equal(newStocks);
-    });
-
-    [
-        1,
-        "1",
-        [1]
-    ].forEach(notAnImmutableList => {
-        it(`should, for ${JSON.stringify(notAnImmutableList)} in stockModel.setStocks(notAnImmutableList), throw TypeError`, function () {
+    describe("Constructor", function () {
+        it("should create StockModel", function () {
             //    given
             //    when
             let stockModel = new StockModel();
 
             //    then
-            chai.expect(stockModel.setStocks.bind(stockModel, notAnImmutableList)).to.throw(TypeError);
+            chai.expect(stockModel).to.be.an.instanceOf(StockModel);
+        });
+    });
+
+    describe("Getters and Setters", function () {
+        it("should contains property 'stocks' that is an immutable List (exposed as getter and setter)", function () {
+            //    given
+            //    when
+            let stockModel = new StockModel();
+
+            //    then
+            chai.expect(stockModel.getStocks()).to.be.an.instanceOf(List);
+            chai.expect(stockModel.getStocks()).to.equal(List());
+
+            let newStocks = List.of(1);
+            stockModel.setStocks(newStocks);
+            chai.expect(stockModel.getStocks()).to.equal(newStocks);
+        });
+
+        [
+            1,
+            "1",
+            [1]
+        ].forEach(notAnImmutableList => {
+            it(`should, for ${JSON.stringify(notAnImmutableList)} in stockModel.setStocks(notAnImmutableList), throw TypeError`, function () {
+                //    given
+                //    when
+                let stockModel = new StockModel();
+
+                //    then
+                chai.expect(stockModel.setStocks.bind(stockModel, notAnImmutableList)).to.throw(TypeError);
+            });
         });
     });
 });
