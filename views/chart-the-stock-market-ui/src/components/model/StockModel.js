8996af7e8c75445e27aad64cc9f5cbb7b5460576
H.T. Koo <h1017760@connect.hku.hk>
2017-10-09 08:28:34 +0800

    added stocks, which is an immutable.List, as property to StockModel FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- views/chart-the-stock-market-ui/src/components/model/StockModel.js
+++ views/chart-the-stock-market-ui/src/components/model/StockModel.js
@@ -1,2 +1,7 @@
+import {List} from 'immutable';
+
 export default class StockModel {
+    constructor(){
+        this.stocks = List();
+    }
 }
--- views/chart-the-stock-market-ui/src/unit-tests/StockModel.test.js
+++ views/chart-the-stock-market-ui/src/unit-tests/StockModel.test.js
@@ -2,6 +2,8 @@ import chai from 'chai';
 
 import StockModel from '../components/model/StockModel';
 
+import {List} from 'immutable';
+
 describe("StockModel", function () {
     it("should create StockModel", function () {
         //    given
@@ -10,5 +12,14 @@ describe("StockModel", function () {
 
         //    then
         chai.expect(stockModel).to.be.an.instanceOf(StockModel);
+    });
+
+    it("should contains property 'stocks' that is an immutable List", function () {
+        //    given
+        //    when
+        let stockModel = new StockModel();
+
+        //    then
+        chai.expect(stockModel.stocks).to.be.an.instanceOf(List);
     });
 });
