91ecbf6f8098107a188e8d0ebfaa4fa745248146
H.T. Koo <h1017760@connect.hku.hk>
2017-10-10 08:50:39 +0800

    refactored StocksModel.js FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- views/chart-the-stock-market-ui/src/components/model/StocksModel.js
+++ views/chart-the-stock-market-ui/src/components/model/StocksModel.js
@@ -3,19 +3,20 @@ import {List} from 'immutable';
 export default class StocksModel {
     constructor(stocks = List()) {
         let _stocks;
+        setStocks(stocks);
 
         this.getStocks = () => {
             return _stocks;
         };
 
-        this.setStocks = stocks => {
+        this.setStocks = setStocks;
+
+        function setStocks(stocks) {
             if (List.isList(stocks)) {
                 _stocks = stocks;
             } else {
                 throw new TypeError("Passed in stocks should be an immutable.List")
             }
-        };
-
-        this.setStocks(stocks);
+        }
     }
 }
