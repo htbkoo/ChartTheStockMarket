8a0f1c34f9c85eb21666bce02f1e8521431bb916
H.T. Koo <h1017760@connect.hku.hk>
2017-10-17 20:09:57 +0800

    server - added test index.test.js for index.js (asserted redirection to index.html) FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

---
+++ test/unit-tests/routes/index.test.js
@@ -0,0 +1,23 @@
+import chai from "chai";
+
+import index from "../../../routes/index";
+
+let request = require('supertest');
+let server = require('../../../app');
+
+describe('routes', function () {
+    describe("index", function () {
+        describe('GET /', function () {
+            it('should return a default string', function () {
+                return request(server)
+                    .get('/')
+                    .set('Accept', 'application/json')
+                    .expect(302)
+                    .expect('Location', /index.html/)
+                    .then(res => {
+                        chai.expect(res.header['location']).to.have.string("index.html");
+                    });
+            });
+        });
+    });
+});
