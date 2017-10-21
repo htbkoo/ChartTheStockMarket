f4f8b5f19bb9fa5a94b5c978fd0aed272c335346
H.T. Koo <h1017760@connect.hku.hk>
2017-10-22 01:31:41 +0800

    server - created unit test pemCertificateCreator.test.js FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

---
+++ test/unit-tests/services/pemCertificateCreator.test.js
@@ -0,0 +1,40 @@
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import {sinonTest} from "../../test-utils/sinonWithSinonTest";
+import pem from "pem-promise";
+
+import pemCertificateCreator from "../../../services/pemCertificateCreator";
+
+chai.use(chaiAsPromised);
+
+describe('services', function () {
+    describe("pemCertificateCreator", function () {
+        describe('promiseCreate', function () {
+            it('should get a promise with keys={serviceKey,certifcate} created by promiseCreate', sinonTest(function () {
+                // given
+                let mockCertificate = "someCert";
+                this.stub(pem, "createCertificate").returns(Promise.resolve(mockCertificate));
+
+                // when
+                let promise = pemCertificateCreator.promiseCreate();
+
+                // then
+                return promise.then(response =>
+                    chai.expect(response).to.equal(mockCertificate)
+                );
+            }));
+
+            it('should propogate the error from pem.createCertificate() when promiseCreate()', sinonTest(function () {
+                // given
+                let expectedError = new Error("Could not find openssl on your system on this path: openssl");
+                this.stub(pem, "createCertificate").returns(Promise.reject(expectedError));
+
+                // when
+                let promise = pemCertificateCreator.promiseCreate();
+
+                // then
+                return chai.expect(promise).to.eventually.be.rejectedWith(expectedError);
+            }));
+        });
+    });
+});
