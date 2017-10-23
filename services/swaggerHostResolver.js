588f25afdf68033055c51a95a6e349cbe225c59e
H.T. Koo <h1017760@connect.hku.hk>
2017-10-24 07:51:55 +0800

    server - updated swaggerHostResolver.js to handle different cases; added more test cases in swaggerHostResolver.test.js FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- services/swaggerHostResolver.js
+++ services/swaggerHostResolver.js
@@ -1,5 +1,11 @@
 module.exports = {
     resolve() {
-        return "localhost:3000";
+        const host = process.env.SWAGGER_HOST,
+            optionalPort = isSwaggerPortRequired() ? `:${process.env.PORT}` : "";
+        return `${host}${optionalPort}`;
     }
 };
+
+function isSwaggerPortRequired() {
+    return process.env.SWAGGER_REQUIRES_PORT && "true" === process.env.SWAGGER_REQUIRES_PORT.toLocaleLowerCase();
+}
--- test/unit-tests/services/swaggerHostResolver.test.js
+++ test/unit-tests/services/swaggerHostResolver.test.js
@@ -5,28 +5,98 @@ import swaggerHostResolver from "../../../services/swaggerHostResolver";
 const originalProcessEnv = process.env;
 
 describe('services', function () {
+    function cloneOf(obj) {
+        return {...obj};
+    }
+
     describe("swaggerHostResolver", function () {
         beforeEach(function () {
             if (originalProcessEnv) {
-                process.env = {...originalProcessEnv};
+                process.env = cloneOf(originalProcessEnv);
             } else {
                 process.env = originalProcessEnv;
             }
         });
 
         describe('resolve', function () {
-            it('should resolve() to get "localhost:3000" given process.env.SWAGGER_HOST=localhost, process.env.SWAGGER_REQUIRES_PORT=true and process.env.PORT=3000', function () {
-                // given
-                process.env.SWAGGER_HOST = "localhost";
-                process.env.SWAGGER_REQUIRES_PORT = true;
-                process.env.PORT = 3000;
+            [
+                {
+                    mockProcessEnv: {
+                        SWAGGER_HOST: "localhost",
+                        SWAGGER_REQUIRES_PORT: "true",
+                        PORT: "3000"
+                    },
+                    expectedHost: "localhost:3000"
+                },
+                {
+                    mockProcessEnv: {
+                        SWAGGER_HOST: "localhost",
+                        SWAGGER_REQUIRES_PORT: "false",
+                        PORT: "3000"
+                    },
+                    expectedHost: "localhost"
+                },
+                {
+                    mockProcessEnv: {
+                        SWAGGER_HOST: "otherhost",
+                        SWAGGER_REQUIRES_PORT: "true",
+                        PORT: "3000"
+                    },
+                    expectedHost: "otherhost:3000"
+                },
+                {
+                    mockProcessEnv: {
+                        SWAGGER_HOST: "otherhost",
+                        SWAGGER_REQUIRES_PORT: "false",
+                        PORT: "3000"
+                    },
+                    expectedHost: "otherhost"
+                },
+                {
+                    mockProcessEnv: {
+                        SWAGGER_HOST: "localhost",
+                        SWAGGER_REQUIRES_PORT: "true",
+                        PORT: "5000"
+                    },
+                    expectedHost: "localhost:5000"
+                },
+                {
+                    mockProcessEnv: {
+                        SWAGGER_HOST: "localhost",
+                        SWAGGER_REQUIRES_PORT: "false",
+                        PORT: "5000"
+                    },
+                    expectedHost: "localhost"
+                },
+                {
+                    mockProcessEnv: {
+                        SWAGGER_HOST: "otherhost",
+                        SWAGGER_REQUIRES_PORT: "true",
+                        PORT: "5000"
+                    },
+                    expectedHost: "otherhost:5000"
+                },
+                {
+                    mockProcessEnv: {
+                        SWAGGER_HOST: "otherhost",
+                        SWAGGER_REQUIRES_PORT: "false",
+                        PORT: "5000"
+                    },
+                    expectedHost: "otherhost"
+                },
+            ].forEach(testCase =>
+                it(`should resolve() to get "${testCase.expectedHost}" given process.env includes ${JSON.stringify(testCase.mockProcessEnv)}`, function () {
+                    // given
+                    chai.expect(process.env).to.deep.equal(originalProcessEnv);
+                    Object.assign(process.env, testCase.mockProcessEnv);
 
-                // when
-                let host = swaggerHostResolver.resolve();
+                    // when
+                    let host = swaggerHostResolver.resolve();
 
-                // then
-                chai.expect(host).to.deep.equal("localhost:3000");
-            });
+                    // then
+                    chai.expect(host).to.deep.equal(testCase.expectedHost);
+                })
+            )
         });
     });
 });
