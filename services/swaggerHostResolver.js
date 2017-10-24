eb9511f8eed2b7e1d1ea71280fcef1ae526aab76
H.T. Koo <h1017760@connect.hku.hk>
2017-10-24 08:18:21 +0800

    server - added edge cases handling in swaggerHostResolver.js FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- services/swaggerHostResolver.js
+++ services/swaggerHostResolver.js
@@ -1,11 +1,16 @@
+function getDefinedOrElse(optional, defaultValue) {
+    let isDefined = !(typeof optional === "undefined");
+    return isDefined ? optional : defaultValue;
+}
+
 module.exports = {
     resolve() {
-        const host = process.env.SWAGGER_HOST,
+        const host = getDefinedOrElse(process.env.SWAGGER_HOST, "localhost"),
             optionalPort = isSwaggerPortRequired() ? `:${process.env.PORT}` : "";
         return `${host}${optionalPort}`;
     }
 };
 
 function isSwaggerPortRequired() {
-    return process.env.SWAGGER_REQUIRES_PORT && "true" === process.env.SWAGGER_REQUIRES_PORT.toLocaleLowerCase();
+    return process.env.SWAGGER_REQUIRES_PORT && process.env.PORT && "true" === process.env.SWAGGER_REQUIRES_PORT.toLocaleLowerCase();
 }
--- test/unit-tests/services/swaggerHostResolver.test.js
+++ test/unit-tests/services/swaggerHostResolver.test.js
@@ -96,7 +96,58 @@ describe('services', function () {
                     // then
                     chai.expect(host).to.deep.equal(testCase.expectedHost);
                 })
-            )
+            );
+
+            it("should return default host (localhost) if process.env.SWAGGER_HOST is undefined", function () {
+                // given
+                chai.expect(process.env).to.deep.equal(originalProcessEnv);
+
+                const defaultValue = "localhost:4000";
+                process.env.PORT = 4000;
+                process.env.SWAGGER_REQUIRES_PORT = "true";
+
+                chai.expect(process.env).to.not.have.property('SWAGGER_HOST');
+
+                // when
+                let host = swaggerHostResolver.resolve();
+
+                // then
+                chai.expect(host).to.deep.equal(defaultValue);
+            });
+
+            it("should be able to return host without port if process.env.SWAGGER_REQUIRES_PORT is undefined", function () {
+                // given
+                chai.expect(process.env).to.deep.equal(originalProcessEnv);
+
+                const givenHost = "someHost";
+                process.env.SWAGGER_HOST = givenHost;
+                process.env.PORT = 4000;
+
+                chai.expect(process.env).to.not.have.property('SWAGGER_REQUIRES_PORT');
+
+                // when
+                let host = swaggerHostResolver.resolve();
+
+                // then
+                chai.expect(host).to.deep.equal(givenHost);
+            });
+
+            it("should be able to return host without port if process.env.PORT is undefined", function () {
+                // given
+                chai.expect(process.env).to.deep.equal(originalProcessEnv);
+
+                const givenHost = "someHost";
+                process.env.SWAGGER_HOST = givenHost;
+                process.env.SWAGGER_REQUIRES_PORT = "true";
+
+                chai.expect(process.env).to.not.have.property('PORT');
+
+                // when
+                let host = swaggerHostResolver.resolve();
+
+                // then
+                chai.expect(host).to.deep.equal(givenHost);
+            });
         });
     });
 });
