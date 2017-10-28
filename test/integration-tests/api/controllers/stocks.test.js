4d8a1a946354fffce7e29f4ca5e07118cc7b275a
H.T. Koo <h1017760@connect.hku.hk>
2017-10-28 19:40:36 +0800

    server - (failed test to be fixed) fixed part of the test in stocks.test.js; added sinon stubbing of getter for reference FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- test/integration-tests/api/controllers/stocks.test.js
+++ test/integration-tests/api/controllers/stocks.test.js
@@ -1,6 +1,9 @@
-let chai = require('chai');
-let request = require('supertest');
-let server = require('../../../../app');
+import chai from "chai";
+import request from "supertest";
+import {sinonTest} from "../../../test-utils/sinonWithSinonTest";
+
+import server from "../../../../app";
+import StocksManager from "../../../../api/helpers/StocksManager";
 
 describe('api', function () {
     describe('controllers', function () {
@@ -16,6 +19,41 @@ describe('api', function () {
                             chai.expect(res.body).to.be.an('array').that.is.empty
                         );
                 });
+
+                it('should return the array of stocks from stocksManager', sinonTest(function () {
+                    let stock1 = {
+                        getUnderlyingId() {
+                            return "a"
+                        },
+                        spotPrice: 10
+                    }, stock2 = {
+                        getUnderlyingId() {
+                            return "b"
+                        },
+                        spotPrice: 20
+                    }, stocks = [stock1, stock2];
+
+                    this.stub(StocksManager.prototype, "stocks").get(() => stocks);
+
+                    return request(server)
+                        .get('/stocks')
+                        .set('Accept', 'application/json')
+                        .expect('Content-Type', /json/)
+                        .expect(200)
+                        .then(res => {
+                            let json = res.body;
+                            chai.expect(json).to.be.an('array').that.has.lengthOf(stocks.length);
+                            chai.expect(json).to.be.deep.equal([
+                                {
+                                    underlyingId: "a",
+                                    spotPrice: 10
+                                }, {
+                                    underlyingId: "b",
+                                    spotPrice: 20
+                                },
+                            ]);
+                        });
+                }));
             });
         });
     });
