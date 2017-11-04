da616073905ec0200f8b81481682fe1de4e35118
H.T. Koo <h1017760@connect.hku.hk>
2017-11-04 15:49:26 +0800

    server - added assertions in app.test.js FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market

--- test/integration-tests/app.test.js
+++ test/integration-tests/app.test.js
@@ -1,16 +1,22 @@
+let request = require('supertest');
+let chai = require("chai");
+
 let server = require('../../app');
 
-let request = require('supertest');
 
 describe('routes', function () {
     describe("index", function () {
         describe('GET /', function () {
-            it('should return a default string', function () {
+            it('should return a html response', function () {
                 return request(server)
                     .get('/')
-                    .set('Accept', 'application/json')
+                    .set('Accept', 'text/html')
                     .expect(200)
                     .expect('Content-Type', /html/)
+                    .expect(/<!DOCTYPE html>/)
+                    .then(res =>
+                        chai.expect(res.text).to.have.string("<!DOCTYPE html>")
+                    )
             });
         });
     });
