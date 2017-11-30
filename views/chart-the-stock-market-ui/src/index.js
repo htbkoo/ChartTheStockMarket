f6144b9c200d8195bfbe722940a9963d74d69238
H.T. Koo <h1017760@connect.hku.hk>
2017-11-30 14:03:48 +0800

    ui - spiked incorporation of redux asynchronous usage FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market
    --HG--
    branch : dev

--- views/chart-the-stock-market-ui/src/index.js
+++ views/chart-the-stock-market-ui/src/index.js
@@ -11,15 +11,21 @@ import thunkMiddleware from 'redux-thunk';
 import {Provider} from 'react-redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 
+import {actions} from "./redux/reduxActions";
+
 const loggerMiddleware = createLogger();
 
+let store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware
+    )
+));
+
+store.dispatch(actions.fetchStocks());
+
 ReactDOM.render(
-    <Provider store={createStore(reducers, composeWithDevTools(
-        applyMiddleware(
-            thunkMiddleware,
-            loggerMiddleware
-        )
-    ))}>
+    <Provider store={store}>
         <App/>
     </Provider>,
     document.getElementById('root')
--- views/chart-the-stock-market-ui/src/redux/reduxActions.js
+++ views/chart-the-stock-market-ui/src/redux/reduxActions.js
@@ -1,7 +1,13 @@
+import request from "request-promise-native";
+import fetch from "whatwg-fetch";
+
 const types = {
     ADD_STOCK: "ADD_STOCK",
     REMOVE_STOCK: "REMOVE_STOCK",
     REORDER_STOCK: "REORDER_STOCK",
+    GET_STOCKS_REQUEST: "GET_STOCKS_REQUEST",
+    GET_STOCKS_SUCCESS: "GET_STOCKS_SUCCESS",
+    GET_STOCKS_FAILUER: "GET_STOCKS_FAILUER",
 };
 
 const actions = {
@@ -22,6 +28,30 @@ const actions = {
             type: types.REORDER_STOCK,
             oldIndex,
             newIndex
+        }
+    },
+    requestGetStocks() {
+        return {
+            type: types.GET_STOCKS_REQUEST,
+        }
+    },
+    receiveGetStocks(stocks) {
+        return {
+            type: types.GET_STOCKS_SUCCESS,
+            stocks
+        }
+    },
+    fetchStocks() {
+        console.log("at actions.fetchStocks()");
+
+        return dispatch => {
+            console.log("dispatching at fetchStocks()");
+            dispatch(this.requestGetStocks());
+
+            return request("http://localhost:3001/stocks", {json: true})
+                .then(json => json.map(obj => obj.underlyingId),
+                    error => console.log('An error occurred.', error))
+                .then(stocks => dispatch(this.receiveGetStocks(stocks)));
         }
     }
 };
