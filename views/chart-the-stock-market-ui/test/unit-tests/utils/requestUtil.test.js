3732f1c9f5b4dac9f1767703ba3bff9e5cb4f9c8
htbkoo <h1017760@connect.hku.hk>
2017-11-30 18:52:48 +0800

    ui - added utils/requestUtil FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market
    --HG--
    branch : dev

--- views/chart-the-stock-market-ui/test/unit-tests/utils/requestUtil.test.js
+++ views/chart-the-stock-market-ui/test/unit-tests/utils/requestUtil.test.js
@@ -1,26 +1,41 @@
 import sinon from "../../test-util/sinonWithSinonTest";
+import mockery from "mockery";
+import chai from "chai";
+import sinonChai from "sinon-chai";
 
-import requestUtil from "utils/requestUtil";
+chai.use(sinonChai);
 
-describe("requestUtil", function(){
-    afterEach(function () {
-        requestUtil.__ResetDependency__('request');
+describe("requestUtil", function () {
+    let mockRequest;
+
+    beforeEach(function (done) {
+        mockery.enable({
+            warnOnReplace: false,
+            warnOnUnregistered: false,
+            useCleanCache: true
+        });
+
+        mockRequest = sinon.spy();
+        mockery.registerMock('request-promise-native', mockRequest);
+
+        done();
     });
 
-    xit("should act as an adpater to the fetch API and expose for easier mocking", function () {
+    afterEach(function (done) {
+        mockery.disable();
+        mockery.deregisterAll();
+        done();
     });
 
     it("should act as an adpater to the request library and expose for easier mocking", sinon.test(function () {
         // given
-        const mockRequest = this.mock();
-        let aUrl = "url", someOptions = {};
-        mockRequest.withArgs(aUrl, someOptions).once();
-        requestUtil.__Rewire__('request', mockRequest);
+        const requestUtil = require("utils/requestUtil");
+        const aUrl = "url", someOptions = {};
 
         // when
         requestUtil.request(aUrl, someOptions);
 
         // then
-        mockRequest.verify();
+        chai.expect(mockRequest).to.have.been.calledWith(aUrl, someOptions);
     }));
 });
