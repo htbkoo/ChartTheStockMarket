fda3bd40af8bd6e45f1237628a50b23b0d3b7885
htbkoo <h1017760@connect.hku.hk>
2017-11-30 19:36:55 +0800

    ui - reverted unused spike part (react-app-rewired, explicit babel dependencies) FreeCodeCamp - Dynamic Web Application Projects - Chart the Stock Market
    --HG--
    branch : dev

--- views/chart-the-stock-market-ui/config-overrides.js
+++
@@ -1,8 +0,0 @@
-const {injectBabelPlugin} = require('react-app-rewired');
-
-module.exports = function override(config, env) {
-    // add a plugin
-    config = injectBabelPlugin('emotion/babel', config);
-
-    return config;
-};
